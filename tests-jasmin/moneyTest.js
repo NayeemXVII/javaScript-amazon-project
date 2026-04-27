b05aed05636f5bd7f347f62c90463b2e9080df0e
Nayeem <nayeem09624@gmail.com>
2026-04-27 15:52:26 +0600

    remove defult test and add money test

---
+++ tests-jasmin/moneyTest.js
@@ -0,0 +1,15 @@
+import { priceCentsFix } from "../scripts/utils/money.js";
+
+describe('Test Suite: priceCentsFix', () => {
+    it('convert cents into dollars', () => {
+        expect(priceCentsFix(2095)).toEqual('20.95');
+    });
+
+    it('works with 0', () => {
+        expect(priceCentsFix(0)).toEqual('0.00');
+    });
+
+    it('rounds up to the nearest cents', () => {
+        expect(priceCentsFix(2000.5)).toEqual('20.01');
+    });
+});
